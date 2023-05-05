9357c42db6a5a7dd608feb9634e133376dd85fd8
drathodd <75549623+drathodd@users.noreply.github.com>
2023-05-05 13:30:16 -0400

    Update Q1-1_stringisASCII.cpp

--- Q1-1_stringisASCII.cpp
+++ Q1-1_stringisASCII.cpp
@@ -3,36 +3,48 @@ if a string has all unique characters. What if you
 cannot use additional data structure? */
 
 #include <stdio.h>
+#include <string>
+#include <iostream>
 
+#define CHARSETSIZE 128
 
 using namespace std;
 
 bool isUnique(string str)
 {
-  bool ret = false;
-  
-  //In case of ASCII, any string longer than 128 character must have repeatition
-  if(str.length() > 128)
-    return ret;
-  else
-  {
-    int[128] numCharacters;
-    for(int it = 0; it < str.length(); it++)
-    {
-      int count = numCharacter[(int)str.getCharAt(it)]++;
-      if(count > 1)
-        return ret;
-    }
-    ret = true;
-  }
- 
-  return ret;
+	bool ret = false;
+
+	//In case of ASCII, any string longer than 128 character must have repeatition
+	if (str.length() > 128)
+		return ret;
+	else
+	{
+		int* numCharacters = (int*)malloc(CHARSETSIZE * sizeof(int));
+		for (int i = 0; i < CHARSETSIZE; i++)
+		{
+			numCharacters[i] = 0;
+		}
+		for (int it = 0; it < str.length(); it++)
+		{
+			int count = ++numCharacters[(int)str[it]];
+			if (count > 1)
+				return ret;
+		}
+		ret = true;
+	}
+
+	return ret;
 }
 
-void main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-  if(argc > 1)
-    string str = argv[1];
-  bool ret = isUnique(str);
-  cout << "String is " << ret ? "" : "not" << " unique";
+	string str;
+	if (argc > 1)
+	{
+		str = argv[1];
+	}
+		
+	bool ret = isUnique(str);
+	cout << "String is" << (ret ? "" : " not") << " unique";
+	return 0;
 }
