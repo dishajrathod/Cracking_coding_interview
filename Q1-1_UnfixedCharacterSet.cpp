0008e1355a813f97442e13fe7392978e41582680
drathodd <75549623+drathodd@users.noreply.github.com>
2023-05-05 13:56:44 -0400

    Update Q1-1_UnfixedCharacterSet.cpp

--- Q1-1_UnfixedCharacterSet.cpp
+++ Q1-1_UnfixedCharacterSet.cpp
@@ -1,1 +1,32 @@
+/* Character set is unfixed(unicode), we may have to compare every single
+character with the other to make sure uniqueness.
+TC is O(n^2)
+SC is O(1) */
 
+include <iostream>
+include <string>
+
+using namespace std;
+
+bool isUnique(string str)
+{
+  bool ret = false;
+  
+  for(int it = 0; it < str.length(); it++)
+  {
+  }
+  
+  return ret;
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc > 1)
+  {
+    string str = argv[1];
+  }
+  
+  bool ret = isUnique(str);
+  cout << "String is" << (ret ? "" : " not ") << "unique"
+  return 0;
+}
