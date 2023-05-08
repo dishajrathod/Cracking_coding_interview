4b1fec22f470ed52c18341e06523c2bc3a27d17c
drathodd <75549623+drathodd@users.noreply.github.com>
2023-05-08 14:39:59 -0400

    Update and rename Q1-1.hpp to Q1-1_RawHashTable.hpp

---
+++ Q1-1_RawHashTable.hpp
@@ -0,0 +1,13 @@
+/* c++ std::unordered_map is a standard hashtable used YET,
+make a hashtable for giggles 
+Amortized TC O(1) 
+includes hash function to create keys
+insert, search, delete values
+Collision can occur but like who cares */
+
+class HashTable:
+{
+public:
+  HashTable();
+  ~HashTable();
+};
